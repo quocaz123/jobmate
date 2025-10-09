f9c3278839558d3ed8792060478ac52384080efe
quocaz123 <quocthangbinh234@gmail.com>
2025-10-09 12:20:10 +0700

    Khởi tạo dự án

---
+++ src/main/java/com/quokka/jobmate_connect/JobmateConnectApplication.java
@@ -0,0 +1,13 @@
+package com.quokka.jobmate_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmateConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmateConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quokka/jobmate_connect/JobmateConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quokka.jobmate_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmateConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
